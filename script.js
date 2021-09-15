80f77d98e683c6b4ef89e6dc28b6413ab6f5bfab
salatielatenco <carlossalatielatenco@gmail.com>
2021-09-15 09:27:51 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+
+
+//1 ejercicio  
+
+//for (let index = 0; index <10; index++) {
+  //      console.log(index);
+        
+   // }
+
+    
+    //2 ejericio 
+   // for (let index = 50; index <151; index++) {
+       // console.log(index);
+        
+    //}
+
+    
+    //3 ejercicio 
+    //for (let index = 8; index <41; ) {
+      //  index+2;
+       // console.log(index);
+        
+    //}
+    
+    //ejericicio 4
+
+
+    //ejericio 5
+/*
+
+        let texto=prompt("ingresa un numero");
+        let contadorvocales=0;
+        let vocales=['a' ,'e','i','o','u','A','E','I','O','U']
+
+        for (let i = 0; i< texto.length; i++) {
+            if (vocales.indexOf(texto[i])>=0) {
+                ++contadorvocales;
+            }
+        
+        }
+        document.write(contadorvocales);
+    
+
+*/
+/*
+    //ejericicio 6
+    
+        let suma=0;
+        numero=50
+        suma=numero*(numero+1)/2
+        document.write(suma)
+
+    
+*/
+
+    //ejercicicio 7
+    
+        let numero=prompt("ingresa un numero");
+        numero=parseInt(numero)
+        let total = 1; 
+        for (i=1; i<=numero; i++) {
+            total = total * i; 
+        }
+        document.write(total); 
+    
+
